50ae746432b135115b0df050ca3214846b7e181e
limaries30 <limaries30@gmail.com>
2020-09-13 16:25:29 +0900

    FIX 변수명 정정 1. wheel => motor

--- ScatCar/modules/wheels.py
+++ ScatCar/modules/wheels.py
@@ -3,8 +3,8 @@ from motors import ServoMotor,DCMotor,Motor
 class FrontWheels:
     
     def __init__(self,motor:Motor):
-        motorLeft = wheelType
-        motorRight= wheelType
+        motorLeft = motor
+        motorRight= motor
 
 class BackWheels:
     
